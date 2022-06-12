8e8b6935e8ed7a20fb6ab7009fd3cf2a6737df57
Alex Dieudonne <alexdieudonne02@gmail.com>
2022-06-12 22:16:53 +0200

    add articles page

--- www/Controller/Admin.class.php
+++ www/Controller/Admin.class.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 session_start();
 
 use App\Core\CleanWords;
-use App\Core\Sql; 
+use App\Core\Sql;
 //use App\Core\SqlPDO;
 use App\Core\Verificator;
 use App\Core\View;
@@ -36,6 +36,8 @@ class Admin
         } else if (count($explode_url) > 2 && $explode_url[2] == "settings") {
             $view = new View("settings", "back");
             $view->assign("user", $user);
+        } else if (count($explode_url) > 2 && $explode_url[2] == "articles") {
+            $view = new View("articles", "back");
         }
 
         // $view = new View("back_home", "back");
@@ -45,23 +47,23 @@ class Admin
         if (!empty($_POST)) {
             //var_dump($_POST);
             $user->logout();
-        } 
-     
+        }
+
         $sql = Sql::getInstance()->prepare("SELECT * FROM esgi_user");
         $sql->execute();
         $result = $sql->fetchAll();
-        
+
         $request = new MySqlBuilder();
         $sql2 = $request
             ->select('esgi_user', ['id', 'firstname', 'lastname', 'email', 'status', 'id_role'])
             ->where('id', $_SESSION['id'])
             ->limit(0, 1)
             ->getQuery();
-        
+
         //var_dump(Sql::getInstance()->query($sql)->fetchALL(\PDO::FETCH_CLASS, 'App\Model\User'));
-        
+
         $poo = (new MySqlBuilder())
-            ->select('esgi_user', ['*'] )
+            ->select('esgi_user', ['*'])
             ->where('id', $_SESSION['id'])
             ->limit(0, 1)
             ->getQuery();
@@ -70,26 +72,25 @@ class Admin
             ->fetchALL(\PDO::FETCH_CLASS, 'App\Model\User');
 
         //var_dump($result2);
-        
-       
+
+
         $class = BUILDER;
         $queryBuilder = new $class();
-        
+
         $lol = $this->test($queryBuilder, $_SESSION['id']);
         $kok = Sql::getInstance()->query($lol)->fetchAll();
         //var_dump($kok);
 
         //var_dump( $this->sendUploadedFileToDB($queryBuilder, $fileName, $id_user, $id_site));
-        
+
         //var_dump($_POST);
-        if(!empty($_POST['submit']))
-        {
-            if(!empty($_FILES)) {
+        if (!empty($_POST['submit'])) {
+            if (!empty($_FILES)) {
                 $this->uploadFile();
                 unset($_POST['submit']);
             }
         }
-        
+
         /*
         $query = $queryBuilder
             ->insert('esgi_file', '',[$fileName, $id_user, $id_site])
@@ -98,26 +99,27 @@ class Admin
         var_dump($query)
         */
         $view->assign("result", $result);
-        
+
 
         unset($_FILES['file']);
         unset($_FILES['fileToUpload']);
-        
-        
+
+
         // $view2 = new View("product", "back");
-       
-    
-        
+
+
+
     }
 
 
-    public function test(QueryBuilder $queryBuilder , $id){
+    public function test(QueryBuilder $queryBuilder, $id)
+    {
         $query = $queryBuilder
             ->select('esgi_user', ['*'])
             ->where('id', $id)
             ->limit(0, 1)
             ->getQuery();
-       
+
         return $query;
     }
 
@@ -129,7 +131,8 @@ class Admin
         return Sql::getInstance()->query($query) ? true : false;
     }
 
-    public function uploadFile() {
+    public function uploadFile()
+    {
         $file = $_FILES['fileToUpload'];
         $fileName = $file['name'];
         $fileTmpName = $file['tmp_name'];
@@ -162,14 +165,15 @@ class Admin
     }
 
 
-    public function coucou(){
+    public function coucou()
+    {
         echo "coucou";
     }
 
-    public function client() {
+    public function client()
+    {
         echo "Ceci";
         $view = new View("front_website", "back");
         $view->assign("user", $user);
-
     }
 }
